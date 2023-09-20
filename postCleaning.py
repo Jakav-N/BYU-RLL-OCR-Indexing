c1167f8eb74e46e4e9c66faa2406894a8dbb07f6
Jakav-N <115905857+Jakav-N@users.noreply.github.com>
2023-09-20 17:32:09 -0600

    Minor post-OCR improvements

--- postCleaning.py
+++ postCleaning.py
@@ -20,11 +20,11 @@ text = re.sub(r"\u00BB", '', text)         #»
 #Characters to be deleted
 text = re.sub(r"[|_]", "", text)
 
-#Deal with misplaced characters
-text = re.sub(r"(\b\S+)([\"])(\S+\b)", "", text)
+#Deal with misplaced characters (no matches)
+##text = re.sub(r"(\b\S+)([\"])(\S+\b)", "", text)
 
 #Characters at line beginnings
-text = re.sub(r"^(?!-sec|--- PAGE)[^A-Za-z\n]{1,2}", "", text, flags=re.MULTILINE)
+text = re.sub(r"^(?!-sec|--- PAGE)[^A-Za-z0-9\n]{1,2}", "", text, flags=re.MULTILINE)
 
 
 with open("skagit-transcribed-processed.txt", "w+", encoding="utf-8") as file:
